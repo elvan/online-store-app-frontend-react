71164bb432d8f4784f17a2c8c87bfc6417f76d42
Elvan H <elvanhd@gmail.com>
2022-01-05 06:46:26 +0700

    instalasi react-bootstrap dan bootswatch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Footer from './app/components/Footer';
+import Header from './app/components/Header';
 
-function App() {
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <>
+      <Header />
+      <Container>
         <h1>Online Store</h1>
-      </header>
-    </div>
+      </Container>
+      <Footer />
+    </>
   );
 }
-
-export default App;
