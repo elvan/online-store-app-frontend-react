e2e4ee2a62b3d438fcf5b63b02789c194182612c
Elvan H <elvanhd@gmail.com>
2022-01-05 18:46:17 +0700

    mengambil data dari react

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import products from '../../seeds/products';
 import ProductItem from '../products/ProductItem';
 
+const BACKEND_API = process.env.REACT_APP_BACKEND_API;
+
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get(`${BACKEND_API}/products`);
+
+        setProducts(data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Featured Products</h1>
       <Row>
-        {products.map((product) => (
+        {products.map((/** @type {any} */ product) => (
           <Col className='my-3' sm={12} md={6} lg={4} xl={4} xxl={3}>
             <ProductItem key={product._id} product={product} />
           </Col>
--- src/features/products/ProductPage.jsx
+++ src/features/products/ProductPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import {
   Breadcrumb,
   Button,
@@ -9,17 +10,36 @@ import {
 } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
-import products from '../../seeds/products';
 import Rating from '../../shared/Rating';
 
+const BACKEND_API = process.env.REACT_APP_BACKEND_API;
+
 const ProductPage = ({ match }) => {
-  const product = products.find((product) => product._id === match.params.id);
+  /** @type {[any, React.Dispatch<any>]} */
+  const [product, setProduct] = useState(null);
+
+  const productId = match.params.id;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get(
+          `${BACKEND_API}/products/${productId}`
+        );
+
+        setProduct(data.product);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   let statusText = '';
 
-  // @ts-ignore
-  if (product?.countInStock > 0) {
-    statusText = `In Stock: ${product?.countInStock}`;
+  if (product.countInStock > 0) {
+    statusText = `In Stock: ${product.countInStock}`;
   } else {
     statusText = 'Out of Stock';
   }
@@ -28,39 +48,39 @@ const ProductPage = ({ match }) => {
     <>
       <Row>
         <Col xs={12} md={6} className='mb-sm-3 mb-md-0'>
-          <Image src={product?.image} alt={product?.name} fluid />
+          <Image src={product.image} alt={product.name} fluid />
         </Col>
         <Col xs={12} md={6}>
           <Breadcrumb className='ms-3'>
             <LinkContainer to='/'>
               <Breadcrumb.Item>Home</Breadcrumb.Item>
             </LinkContainer>
-            <LinkContainer to={`/categories/${product?.category}`}>
-              <Breadcrumb.Item>{product?.category}</Breadcrumb.Item>
+            <LinkContainer to={`/categories/${product.category}`}>
+              <Breadcrumb.Item>{product.category}</Breadcrumb.Item>
             </LinkContainer>
-            <LinkContainer to={`/brands/${product?.brand}`}>
-              <Breadcrumb.Item active>{product?.brand}</Breadcrumb.Item>
+            <LinkContainer to={`/brands/${product.brand}`}>
+              <Breadcrumb.Item active>{product.brand}</Breadcrumb.Item>
             </LinkContainer>
           </Breadcrumb>
           <ListGroup variant='flush'>
             <ListGroup.Item>
-              <h1>{product?.brand}</h1>
-              <h2>{product?.name}</h2>
+              <h1>{product.brand}</h1>
+              <h2>{product.name}</h2>
             </ListGroup.Item>
             <ListGroup.Item>
               <Rating
-                rating={product?.rating}
-                numberOfReviews={product?.numberOfReviews}
+                rating={product.rating}
+                numberOfReviews={product.numberOfReviews}
               />
             </ListGroup.Item>
             <ListGroup.Item>
               <h5>
-                <strong>Price:</strong> {product?.price}
+                <strong>Price:</strong> {product.price}
               </h5>
             </ListGroup.Item>
             <ListGroup.Item>
               <h5>Product Information</h5>
-              <p>{product?.description}</p>
+              <p>{product.description}</p>
               <p>
                 <strong>{statusText}</strong>
               </p>
@@ -71,7 +91,7 @@ const ProductPage = ({ match }) => {
                   variant='primary'
                   size='lg'
                   type='button'
-                  disabled={product?.countInStock === 0}
+                  disabled={product.countInStock === 0}
                 >
                   Add to Cart
                 </Button>
